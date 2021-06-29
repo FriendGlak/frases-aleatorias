ca99651f6f81f3c2fe2ca0bd083ad3b840c97a34
FriendGlak <camilojheans@gmail.com>
2021-06-28 21:32:04 -0500

    Frases aleatorias.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React, { useState, useEffect } from 'react'
+
+let frasesBD = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
 
 function App() {
+  const [frase, setFrase] = useState('The greatest glory in living lies not in never falling, but in rising every time we fall.')
+  const [autor, setAutor] = useState('Nelson Mandela')
+  const [randomNumber, setRandomInt] = useState(0)
+  const [frasesArray, setFrasesArray] = useState(null)
+
+  const fetchFrases = async (url) => {
+    const response = await fetch(url)
+    const parseJSON = await response.json()
+    setFrasesArray(parseJSON.quotes)
+    console.log(parseJSON)
+  }
+
+  useEffect(() => {
+    fetchFrases(frasesBD)
+  }, [frasesBD])
+
+  const cambiarFraseAutor = () => {
+    let randomInt = Math.floor(frasesArray.length * Math.random())
+    setRandomInt(randomInt)
+    setFrase(frasesArray[randomInt].quote)
+    setAutor(frasesArray[randomInt].author)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container-boxfrase">
+          <div className="container-textfrase">
+            <p id="text"> "{frase}" </p>
+            <p id="autor">{autor}</p>
+          </div>
+          <button className="button" onClick={() => cambiarFraseAutor()}>Siguiente</button>
+        </div>
       </header>
     </div>
   );
